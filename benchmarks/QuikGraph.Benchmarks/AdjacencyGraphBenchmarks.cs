c36d2b7b0f38d027922cc65b660ca72a724ffd20
Iacentis <CarlJFandersson@gmail.com>
2026-01-22 22:39:58 +0100

    Benchmarks and GitHub actions to run them

---
+++ benchmarks/QuikGraph.Benchmarks/AdjacencyGraphBenchmarks.cs
@@ -0,0 +1,48 @@
+﻿using System.Linq;
+using BenchmarkDotNet.Attributes;
+using QuikGraph;
+
+namespace QuikGraph.Benchmarks
+{
+    [MemoryDiagnoser]
+    public class AdjacencyGraphBenchmarks
+    {
+        private AdjacencyGraph<int, Edge<int>> _graph;
+
+        [Params(100, 1000)]
+        public int VertexCount;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            _graph = new AdjacencyGraph<int, Edge<int>>();
+            for (int i = 0; i < VertexCount; i++)
+            {
+                _graph.AddVertex(i);
+            }
+
+            for (int i = 0; i < VertexCount - 1; i++)
+            {
+                _graph.AddEdge(new Edge<int>(i, i + 1));
+            }
+        }
+
+        [Benchmark]
+        public bool ContainsVertex()
+        {
+            return _graph.ContainsVertex(VertexCount / 2);
+        }
+
+        [Benchmark]
+        public int OutDegree()
+        {
+            return _graph.OutDegree(VertexCount / 2);
+        }
+
+        [Benchmark]
+        public Edge<int>[] OutEdges()
+        {
+            return _graph.OutEdges(VertexCount / 2).ToArray();
+        }
+    }
+}
---
+++ benchmarks/QuikGraph.Data.Benchmarks/DataSetBenchmarks.cs
@@ -0,0 +1,42 @@
+﻿using System.Data;
+using BenchmarkDotNet.Attributes;
+using QuikGraph.Data;
+
+namespace QuikGraph.Data.Benchmarks
+{
+    [MemoryDiagnoser]
+    public class DataSetBenchmarks
+    {
+        private DataSet _dataSet;
+
+        [Params(10, 50)]
+        public int TableCount;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            _dataSet = new DataSet();
+            for (int i = 0; i < TableCount; i++)
+            {
+                var table = new DataTable($"Table{i}");
+                table.Columns.Add("Id", typeof(int));
+                _dataSet.Tables.Add(table);
+            }
+
+            for (int i = 0; i < TableCount - 1; i++)
+            {
+                var relation = new DataRelation(
+                    $"Relation{i}",
+                    _dataSet.Tables[i].Columns["Id"],
+                    _dataSet.Tables[i + 1].Columns["Id"]);
+                _dataSet.Relations.Add(relation);
+            }
+        }
+
+        [Benchmark]
+        public DataSetGraph ToGraph()
+        {
+            return _dataSet.ToGraph();
+        }
+    }
+}
